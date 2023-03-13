35c5533b237565f8fd73debc8d77136a328ffea5
Vivian Chen <vivian.chen1193@gmail.com>
2023-03-13 15:07:55 -0600

    made conditional statements on when to show the address form, payment form, and confirmation of the order

--- src/components/Checkoutform/Checkout/Checkout.jsx
+++ src/components/Checkoutform/Checkout/Checkout.jsx
@@ -1,16 +1,21 @@
 import React, { useState } from 'react';
 import { Paper, Stepper, Step, StepLabel, Typography, CircularProgress, Divider, Button } from '@material-ui/core';
+import useStyles from './styles';
 
 
 const steps = ['Shipping Address', 'Payment Details']
 const Checkout = () => {
 
     const [activeStep, setActiveStep] = useState(0);
+    const classes = useStyles();
+
+    const Form = () => activeStep === 0 ? <AddressForm /> : <PaymentForm />
 
   return (
     <>
         <div className={classes.toolbar} />
         <main className={classes.layout}>
+            {/* section that displays the steps for entering the shipping address and then payment details */}
             <Paper className={classes.paper}>
                 <Typography variant='h4' align='center'>Checkout</Typography>
                 <Stepper activeStep={activeStep} className={classes.stepper} >
@@ -20,7 +25,10 @@ const Checkout = () => {
                         </Step>
                     ))}
                 </Stepper>
+                {/* if the active step is on the last step, then show the confirmation componenent */}
+                {activeStep === steps.length ? <Confirmation /> : <Form /> }
             </Paper>
+
         </main>
     </>
   )
--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,4 @@
 export { default as Navbar } from './Navbar/Navbar';
 export { default as Products } from './Products/products';
 export { default as Cart } from './Cart/Cart';
+export { default as Checkout } from './Checkoutform/Checkout/Checkout';
