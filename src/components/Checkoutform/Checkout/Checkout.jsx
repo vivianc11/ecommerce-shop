532c1e213f98b55b4843ead2f8da5333344fee87
Vivian Chen <vivian.chen1193@gmail.com>
2023-03-13 14:58:04 -0600

    set up steps using stepper, steps, and steplabel in checkout

--- src/components/Checkoutform/Checkout/Checkout.jsx
+++ src/components/Checkoutform/Checkout/Checkout.jsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Paper, Stepper, Step, StepLabel, Typography, CircularProgress, Divider, Button } from '@material-ui/core';
 
+
+const steps = ['Shipping Address', 'Payment Details']
 const Checkout = () => {
+
+    const [activeStep, setActiveStep] = useState(0);
+
   return (
-    <div>Checkout</div>
+    <>
+        <div className={classes.toolbar} />
+        <main className={classes.layout}>
+            <Paper className={classes.paper}>
+                <Typography variant='h4' align='center'>Checkout</Typography>
+                <Stepper activeStep={activeStep} className={classes.stepper} >
+                    {steps.map((step) => (
+                        <Step key={step}>
+                            <StepLabel>{step}</StepLabel>
+                        </Step>
+                    ))}
+                </Stepper>
+            </Paper>
+        </main>
+    </>
   )
 }
 
