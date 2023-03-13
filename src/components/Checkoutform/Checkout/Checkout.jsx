30dd12677f67d45f9141fe16e1c1bf022a85eed6
Vivian Chen <vivian.chen1193@gmail.com>
2023-03-13 15:11:18 -0600

    imported addressform and paymentform into checkout.jsx

--- src/components/Checkoutform/Checkout/Checkout.jsx
+++ src/components/Checkoutform/Checkout/Checkout.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { Paper, Stepper, Step, StepLabel, Typography, CircularProgress, Divider, Button } from '@material-ui/core';
 import useStyles from './styles';
+import AddressForm from '../AddressForm';
+import PaymentForm from '../PaymentForm';
 
 
 const steps = ['Shipping Address', 'Payment Details']
