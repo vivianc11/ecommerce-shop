b7346308cc922f42e39a23a3bef7fd3a9387dd9c
Vivian Chen <vivian.chen1193@gmail.com>
2023-03-14 12:27:45 -0600

    passed checkoutToken from checkout.jsx to paymentform.jsx to review.jsx

--- src/components/Checkoutform/Checkout/Checkout.jsx
+++ src/components/Checkoutform/Checkout/Checkout.jsx
@@ -19,7 +19,7 @@ const Checkout = ({ cart }) => {
             try {
                 const token = await commerce.checkout.generateToken(cart.id, { type: 'cart' });
 
-                console.log(token);
+                // console.log(token);
 
                 setcheckoutToken(token);
 
@@ -48,7 +48,7 @@ const Checkout = ({ cart }) => {
         </div>
     );
 
-    const Form = () => activeStep === 0 ? <AddressForm checkoutToken={checkoutToken} next={next} /> : <PaymentForm />
+    const Form = () => activeStep === 0 ? <AddressForm checkoutToken={checkoutToken} next={next} /> : <PaymentForm checkoutToken={checkoutToken}/>
 
   return (
     <>
